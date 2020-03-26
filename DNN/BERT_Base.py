c96a64aed5740dd4bdd1970a86c3387858d7e68b
Lee JE <58355531+Jane096@users.noreply.github.com>
2020-03-26 12:02:28 +0900

    Add files via upload

---
+++ DNN/BERT_Base.py
@@ -0,0 +1,407 @@
+import numpy as np
+import pandas as pd
+import re
+import string
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Input, SpatialDropout1D, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint
+import tensorflow_hub as hub
+#import bert
+# from bert import tokenization
+from final import tokenization
+from nltk.tokenize import word_tokenize
+import nltk
+
+# hyper-parameter
+# Dropout_num = 0
+# learning_rate = 9e-6
+# valid = 0.2
+# epochs_num = 3
+# batch_size_num = 16
+# target_corrected = False
+# target_big_corrected = False
+
+# 하이퍼파라미터 조정
+Dropout_num = 0
+learning_rate = 6e-6
+valid = 0.2
+epochs_num = 3
+batch_size_num = 16
+target_corrected = False
+target_big_corrected = False
+
+train = pd.read_csv("train.csv", encoding='utf-8')
+test = pd.read_csv("test.csv", encoding='utf-8')
+submission = pd.read_csv("sample_submission.csv", encoding='utf-8')
+
+# df=pd.concat([tweet,test])
+# print(df.shape)
+
+def remove_URL(text):
+    url = re.compile(r'https?://\S+|www\.\S+')
+    return url.sub(r'',text)
+
+train['text']=train['text'].apply(lambda x : remove_URL(x))
+test['text']=test['text'].apply(lambda x : remove_URL(x))
+
+def remove_html(text):
+    html=re.compile(r'<.*?>')
+    return html.sub(r'',text)
+
+train['text']=train['text'].apply(lambda x : remove_html(x))
+test['text']=test['text'].apply(lambda x : remove_html(x))
+
+def remove_emoji(text):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+train['text']=train['text'].apply(lambda x: remove_emoji(x))
+test['text']=test['text'].apply(lambda x: remove_emoji(x))
+
+def remove_punct(text):
+    table=str.maketrans('','',string.punctuation)
+    return text.translate(table)
+
+train['text']=train['text'].apply(lambda x : remove_punct(x))
+test['text']=test['text'].apply(lambda x : remove_punct(x))
+
+abbreviations = {
+    "$" : " dollar ",
+    "€" : " euro ",
+    "4ao" : "for adults only",
+    "a.m" : "before midday",
+    "a3" : "anytime anywhere anyplace",
+    "aamof" : "as a matter of fact",
+    "acct" : "account",
+    "adih" : "another day in hell",
+    "afaic" : "as far as i am concerned",
+    "afaict" : "as far as i can tell",
+    "afaik" : "as far as i know",
+    "afair" : "as far as i remember",
+    "afk" : "away from keyboard",
+    "app" : "application",
+    "approx" : "approximately",
+    "apps" : "applications",
+    "asap" : "as soon as possible",
+    "asl" : "age, sex, location",
+    "atk" : "at the keyboard",
+    "ave." : "avenue",
+    "aymm" : "are you my mother",
+    "ayor" : "at your own risk",
+    "b&b" : "bed and breakfast",
+    "b+b" : "bed and breakfast",
+    "b.c" : "before christ",
+    "b2b" : "business to business",
+    "b2c" : "business to customer",
+    "b4" : "before",
+    "b4n" : "bye for now",
+    "b@u" : "back at you",
+    "bae" : "before anyone else",
+    "bak" : "back at keyboard",
+    "bbbg" : "bye bye be good",
+    "bbc" : "british broadcasting corporation",
+    "bbias" : "be back in a second",
+    "bbl" : "be back later",
+    "bbs" : "be back soon",
+    "be4" : "before",
+    "bfn" : "bye for now",
+    "blvd" : "boulevard",
+    "bout" : "about",
+    "brb" : "be right back",
+    "bros" : "brothers",
+    "brt" : "be right there",
+    "bsaaw" : "big smile and a wink",
+    "btw" : "by the way",
+    "bwl" : "bursting with laughter",
+    "c/o" : "care of",
+    "cet" : "central european time",
+    "cf" : "compare",
+    "cia" : "central intelligence agency",
+    "csl" : "can not stop laughing",
+    "cu" : "see you",
+    "cul8r" : "see you later",
+    "cv" : "curriculum vitae",
+    "cwot" : "complete waste of time",
+    "cya" : "see you",
+    "cyt" : "see you tomorrow",
+    "dae" : "does anyone else",
+    "dbmib" : "do not bother me i am busy",
+    "diy" : "do it yourself",
+    "dm" : "direct message",
+    "dwh" : "during work hours",
+    "e123" : "easy as one two three",
+    "eet" : "eastern european time",
+    "eg" : "example",
+    "embm" : "early morning business meeting",
+    "encl" : "enclosed",
+    "encl." : "enclosed",
+    "etc" : "and so on",
+    "faq" : "frequently asked questions",
+    "fawc" : "for anyone who cares",
+    "fb" : "facebook",
+    "fc" : "fingers crossed",
+    "fig" : "figure",
+    "fimh" : "forever in my heart",
+    "ft." : "feet",
+    "ft" : "featuring",
+    "ftl" : "for the loss",
+    "ftw" : "for the win",
+    "fwiw" : "for what it is worth",
+    "fyi" : "for your information",
+    "g9" : "genius",
+    "gahoy" : "get a hold of yourself",
+    "gal" : "get a life",
+    "gcse" : "general certificate of secondary education",
+    "gfn" : "gone for now",
+    "gg" : "good game",
+    "gl" : "good luck",
+    "glhf" : "good luck have fun",
+    "gmt" : "greenwich mean time",
+    "gmta" : "great minds think alike",
+    "gn" : "good night",
+    "g.o.a.t" : "greatest of all time",
+    "goat" : "greatest of all time",
+    "goi" : "get over it",
+    "gps" : "global positioning system",
+    "gr8" : "great",
+    "gratz" : "congratulations",
+    "gyal" : "girl",
+    "h&c" : "hot and cold",
+    "hp" : "horsepower",
+    "hr" : "hour",
+    "hrh" : "his royal highness",
+    "ht" : "height",
+    "ibrb" : "i will be right back",
+    "ic" : "i see",
+    "icq" : "i seek you",
+    "icymi" : "in case you missed it",
+    "idc" : "i do not care",
+    "idgadf" : "i do not give a damn fuck",
+    "idgaf" : "i do not give a fuck",
+    "idk" : "i do not know",
+    "ie" : "that is",
+    "i.e" : "that is",
+    "ifyp" : "i feel your pain",
+    "IG" : "instagram",
+    "iirc" : "if i remember correctly",
+    "ilu" : "i love you",
+    "ily" : "i love you",
+    "imho" : "in my humble opinion",
+    "imo" : "in my opinion",
+    "imu" : "i miss you",
+    "iow" : "in other words",
+    "irl" : "in real life",
+    "j4f" : "just for fun",
+    "jic" : "just in case",
+    "jk" : "just kidding",
+    "jsyk" : "just so you know",
+    "l8r" : "later",
+    "lb" : "pound",
+    "lbs" : "pounds",
+    "ldr" : "long distance relationship",
+    "lmao" : "laugh my ass off",
+    "lmfao" : "laugh my fucking ass off",
+    "lol" : "laughing out loud",
+    "ltd" : "limited",
+    "ltns" : "long time no see",
+    "m8" : "mate",
+    "mf" : "motherfucker",
+    "mfs" : "motherfuckers",
+    "mfw" : "my face when",
+    "mofo" : "motherfucker",
+    "mph" : "miles per hour",
+    "mr" : "mister",
+    "mrw" : "my reaction when",
+    "ms" : "miss",
+    "mte" : "my thoughts exactly",
+    "nagi" : "not a good idea",
+    "nbc" : "national broadcasting company",
+    "nbd" : "not big deal",
+    "nfs" : "not for sale",
+    "ngl" : "not going to lie",
+    "nhs" : "national health service",
+    "nrn" : "no reply necessary",
+    "nsfl" : "not safe for life",
+    "nsfw" : "not safe for work",
+    "nth" : "nice to have",
+    "nvr" : "never",
+    "nyc" : "new york city",
+    "oc" : "original content",
+    "og" : "original",
+    "ohp" : "overhead projector",
+    "oic" : "oh i see",
+    "omdb" : "over my dead body",
+    "omg" : "oh my god",
+    "omw" : "on my way",
+    "p.a" : "per annum",
+    "p.m" : "after midday",
+    "pm" : "prime minister",
+    "poc" : "people of color",
+    "pov" : "point of view",
+    "pp" : "pages",
+    "ppl" : "people",
+    "prw" : "parents are watching",
+    "ps" : "postscript",
+    "pt" : "point",
+    "ptb" : "please text back",
+    "pto" : "please turn over",
+    "qpsa" : "what happens", #"que pasa",
+    "ratchet" : "rude",
+    "rbtl" : "read between the lines",
+    "rlrt" : "real life retweet",
+    "rofl" : "rolling on the floor laughing",
+    "roflol" : "rolling on the floor laughing out loud",
+    "rotflmao" : "rolling on the floor laughing my ass off",
+    "rt" : "retweet",
+    "ruok" : "are you ok",
+    "sfw" : "safe for work",
+    "sk8" : "skate",
+    "smh" : "shake my head",
+    "sq" : "square",
+    "srsly" : "seriously",
+    "ssdd" : "same stuff different day",
+    "tbh" : "to be honest",
+    "tbs" : "tablespooful",
+    "tbsp" : "tablespooful",
+    "tfw" : "that feeling when",
+    "thks" : "thank you",
+    "tho" : "though",
+    "thx" : "thank you",
+    "tia" : "thanks in advance",
+    "til" : "today i learned",
+    "tl;dr" : "too long i did not read",
+    "tldr" : "too long i did not read",
+    "tmb" : "tweet me back",
+    "tntl" : "trying not to laugh",
+    "ttyl" : "talk to you later",
+    "u" : "you",
+    "u2" : "you too",
+    "u4e" : "yours for ever",
+    "utc" : "coordinated universal time",
+    "w/" : "with",
+    "w/o" : "without",
+    "w8" : "wait",
+    "wassup" : "what is up",
+    "wb" : "welcome back",
+    "wtf" : "what the fuck",
+    "wtg" : "way to go",
+    "wtpa" : "where the party at",
+    "wuf" : "where are you from",
+    "wuzup" : "what is up",
+    "wywh" : "wish you were here",
+    "yd" : "yard",
+    "ygtr" : "you got that right",
+    "ynk" : "you never know",
+    "zzz" : "sleeping bored and tired"
+}
+
+def convert_abbrev(word):
+    return abbreviations[word.lower()] if word.lower() in abbreviations.keys() else word
+
+nltk.download('punkt')
+
+def convert_abbrev_in_text(text):
+    tokens = word_tokenize(text)
+    tokens = [convert_abbrev(word) for word in tokens]
+    text = ' '.join(tokens)
+    return text
+
+module_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/1"
+bert_layer = hub.KerasLayer(module_url, trainable=True)
+#
+train["text"] = train["text"].apply(lambda x: convert_abbrev(x))
+test["text"] = test["text"].apply(lambda x: convert_abbrev(x))
+
+test["text"] = test["text"].apply(lambda x: convert_abbrev_in_text(x))
+
+def bert_encode(texts, tokenizer, max_len=512):
+    all_tokens = []
+    all_masks = []
+    all_segments = []
+
+    for text in texts:
+        text = tokenizer.tokenize(text)
+
+        text = text[:max_len - 2]
+        input_sequence = ["[CLS]"] + text + ["[SEP]"]
+        pad_len = max_len - len(input_sequence)
+
+        tokens = tokenizer.convert_tokens_to_ids(input_sequence)
+        tokens += [0] * pad_len
+        pad_masks = [1] * len(input_sequence) + [0] * pad_len
+        segment_ids = [0] * max_len
+
+        all_tokens.append(tokens)
+        all_masks.append(pad_masks)
+        all_segments.append(segment_ids)
+
+    return np.array(all_tokens), np.array(all_masks), np.array(all_segments)
+
+
+def build_model(bert_layer, max_len=512):
+    input_word_ids = Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
+    input_mask = Input(shape=(max_len,), dtype=tf.int32, name="input_mask")
+    segment_ids = Input(shape=(max_len,), dtype=tf.int32, name="segment_ids")
+
+    _, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
+    clf_output = sequence_output[:, 0, :]
+
+    if Dropout_num == 0:
+        # Without Dropout
+        out = Dense(1, activation='sigmoid')(clf_output)
+    else:
+        # With Dropout(Dropout_num), Dropout_num > 0
+        x = Dropout(Dropout_num)(clf_output)
+        out = Dense(1, activation='sigmoid')(x)
+
+    # out = Dense(1, activation='sigmoid')(clf_output)
+
+    model = Model(inputs=[input_word_ids, input_mask, segment_ids], outputs=out)
+    model.compile(Adam(lr=learning_rate), loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model
+
+vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
+do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
+tokenizer = tokenization.FullTokenizer(vocab_file, do_lower_case)
+
+
+train_input = bert_encode(train.text.values, tokenizer, max_len=160)
+test_input = bert_encode(test.text.values, tokenizer, max_len=160)
+train_labels = train.target.values
+
+model_BERT = build_model(bert_layer, max_len=160)
+model_BERT.summary()
+
+checkpoint = ModelCheckpoint('model_BERT.h5', monitor='val_loss', save_best_only=True)
+
+train_history = model_BERT.fit(
+    train_input, train_labels,
+    validation_split = valid,
+    epochs = epochs_num, # recomended 3-5 epochs
+    callbacks=[checkpoint],
+    batch_size = batch_size_num
+)
+
+model_BERT.load_weights('model_BERT.h5')
+test_pred_BERT = model_BERT.predict(test_input)
+test_pred_BERT_int = test_pred_BERT.round().astype('int')
+
+train_pred_BERT = model_BERT.predict(train_input)
+train_pred_BERT_int = train_pred_BERT.round().astype('int')
+
+# 문제점
+# 1. tf-hub 통해서 다운받은 bert layer 작동 안되는 것 같음(내 컴퓨터의 문제?)
+# 2. 텐서플로의 API 변경, gfile은 tf.io 패키지로 옮겨갔다는데 최신 다운받은 bert tokenization 파일에 반영이 안되어있음 -> 뭐임?
+# 3. github에 있는 변경된 코드로 tokenization import 해도 해당 파일의 함수를 불러오지 못함..
+
+# 토크나이징이나 word embedding을 적용하지 않아도 정확도가 80%를 넘기는 걸 보면 bert에서는 적용을 하지 않아도 될 것 같음
